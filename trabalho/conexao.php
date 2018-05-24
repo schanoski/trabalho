dd514a4ff8fd52b26b1d87df618f8fb9f100d007
schanoski <schanoski97@gmail.com>
2018-05-24 17:23:21 -0300

    Update conexao.php

--- trabalho/conexao.php
+++ trabalho/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexao = mysqli_connect("localhost", "root", "sql7ender6ibson") or die("erro ao conectar");
+$conexao = mysqli_connect("localhost", "root", "root") or die("erro ao conectar");
 $banco = mysqli_select_db($conexao, "trabalho") or die(mysqli_error($conexao));
 mysqli_set_charset($conexao, "utf8");
 
